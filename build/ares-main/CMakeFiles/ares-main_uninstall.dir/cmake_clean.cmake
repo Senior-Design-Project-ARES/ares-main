4d10f9180c61c2bf8979f5585049cfb380d25d38
Sam <samaafallon@gmail.com>
2025-10-08 10:52:02 -0600

    hello world ros

---
+++ build/ares-main/CMakeFiles/ares-main_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ares-main_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ares-main_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
