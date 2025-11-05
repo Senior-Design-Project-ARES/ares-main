f70e255e5df7a2bd7151c628654b9e447ec61d2b
Ollie Kaplan <145784706+ockaplan@users.noreply.github.com>
2025-11-05 10:59:14 -0700

    Updated Stanley sim and Pure Pursuit sim (#3)
    Co-authored-by: ockap <ockap@10.203.167.173>

--- models/stanleysim-okaplan/main.m
+++ models/stanleysim-okaplan/main.m
@@ -2,6 +2,10 @@
 
 clear; clc; close all;
 
+%set noise parameters
+minNoise = -0.0000001;
+maxNoise = 0.000001;
+
 %run the simulation
 out = sim("stanleypathtracking.slx");
 
@@ -16,15 +20,19 @@ figure()
 %plot desired trajectory
 scatter(out.waypoints(:,1),out.waypoints(:,2),'Marker','o')
 hold on
-plot(RefPoses(1:100,1),RefPoses(1:100,2));
+plot(RefPoses(1:200,1),RefPoses(1:200,2));
 
 %plot position
 plot(position(:,1),position(:,2),'LineWidth',1)
 scatter(position(1,1),position(2,2),'marker','x','LineWidth',1,'MarkerEdgeColor','green')
 scatter(position(end,1),position(end,2),'marker','x','LineWidth',1,'MarkerEdgeColor','red')
 
+%find time to reach end
+[~,finIndex] = min(vecnorm(position - [ones(height(position),1)*position(end,1) ones(height(position),1)*position(end,2)],2,2));
+tend = out.tout(finIndex);
+
 %make pretty
-title(['Stanley Trajectory, 60s, x_0=' num2str(IC(1)) ', y_0=' num2str(IC(2)) ', \theta_0 =' num2str(IC(3))])
+title(['Stanley Trajectory, ' num2str(tend) 's, x_0=' num2str(IC(1)) ', y_0=' num2str(IC(2)) ', \theta_0 =' num2str(IC(3))])
 legend('Waypoints','Interpolated Path', 'Trajectory', 'Start','End','location','southeast')
 axis equal
 
@@ -34,3 +42,14 @@ plot(out.RefVelocities)
 xlabel("Index")
 ylabel("reference velocity (m/s)")
 title("reference velocities vs index")
+
+%crosstrack error plot
+figure()
+y = lowpass(out.CTError,0.003,0.1);
+plot(out.tout,out.CTError)
+hold on
+plot(out.tout,y,'LineWidth',2)
+xlabel("time (s)")
+ylabel("crosstrack error (m)")
+title("crosstrack error vs. time")
+legend('Data','Data w/ LPF')
