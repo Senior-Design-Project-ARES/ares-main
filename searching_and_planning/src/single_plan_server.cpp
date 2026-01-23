317ab496f9d79729e3825f0f36efb0e36562c0a2
YuKang <yukang.kong.uni@gmail.com>
2026-01-23 11:21:40 -0700

    create server for frontier seraching and planning

---
+++ searching_and_planning/src/single_plan_server.cpp
@@ -0,0 +1,17 @@
+#include "rclcpp/rclcpp.hpp"
+
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+
+  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
+    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+}
