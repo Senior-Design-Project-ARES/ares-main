62bd147bcc87b538d3f373aa8ead8873f9157166
YuKang <yukang.kong.uni@gmail.com>
2026-01-23 09:47:57 -0700

    add all marco into one location

---
+++ searching_and_planning/include/searching_and_planning/core/UsefulMacros.h
@@ -0,0 +1,9 @@
+#ifndef USEFULMACROS_H
+#define USEFULMACROS_H
+
+#define LIDARRADIUS 4.0  // meters
+#define MIN_REGION_LENGTH 1.0 // in meters
+#define ABSULUTE_MIN_REGION_LENGTH 0.1 // in meters
+#define GAP 0.05
+
+#endif 
