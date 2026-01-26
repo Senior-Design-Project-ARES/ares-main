1219a032dde844f0c8953a07b6570e3fabc683cb
YuKang <yukang.kong.uni@gmail.com>
2026-01-25 18:38:38 -0700

    add Macro

--- searching_and_planning/include/searching_and_planning/core/UsefulMacros.h
+++ searching_and_planning/include/searching_and_planning/core/UsefulMacros.h
@@ -5,5 +5,13 @@
 #define MIN_REGION_LENGTH 1.0 // in meters
 #define ABSULUTE_MIN_REGION_LENGTH 0.1 // in meters
 #define GAP 0.05
+#define ROVER_RADIUS 0.15 // in meters
+#define RADIUS_INFLATION 1.5 // inflation for rover radius when adding path obstacles
+#define MAP_WIDTH 100 // in cells
+#define MAP_HEIGHT 100 // in cells
+#define X_MIN 0.0 // in meters
+#define X_MAX 10.0 // in meters
+#define Y_MIN 0.0 // in meters
+#define Y_MAX 10.0 // in meters
 
 #endif 
