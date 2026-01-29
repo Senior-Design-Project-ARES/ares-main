94ba21513d1c6b6c0c177f789aabcc1795df02be
tethracross <vishnuduriseti@gmail.com>
2026-01-29 13:52:21 -0700

    cleaned up main a little bit

--- ares_embedded/firmware/main.cpp
+++ ares_embedded/firmware/main.cpp
@@ -27,67 +27,6 @@ TIM_HandleTypeDef htim12_red_led = {};
 
 
 /**
- * @brief System clock configuration for NUCLEO-H723ZG
- * 
- * System Clock: 520 MHz
- * HSE: 8 MHz (bypass mode)
- * PLL: HSE -> PLL -> System Clock
- * 
- * THIS IS NOT USED, BUT IS HERE FOR REFERENCE
- */
-// void SystemClock_Config(void)
-// {
-//     RCC_ClkInitTypeDef RCC_ClkInitStruct = {}; // used to be {0} -- redefined to see if this fixes errors
-//     RCC_OscInitTypeDef RCC_OscInitStruct = {}; // used to be {0} -- redefined to see if this fixes errors
-//     HAL_StatusTypeDef ret = HAL_OK;
-
-//     // Configure voltage scaling
-//     __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
-//     while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-
-//     // Configure HSE and PLL
-//     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-//     RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-//     RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-//     RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
-//     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-//     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    
-//     RCC_OscInitStruct.PLL.PLLM = 4;
-//     RCC_OscInitStruct.PLL.PLLN = 260;
-//     RCC_OscInitStruct.PLL.PLLFRACN = 0;
-//     RCC_OscInitStruct.PLL.PLLP = 1;
-//     RCC_OscInitStruct.PLL.PLLR = 2;
-//     RCC_OscInitStruct.PLL.PLLQ = 4;
-//     RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-//     RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_1;
-    
-//     ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
-//     if(ret != HAL_OK) {
-//         while(1) {} // Error handler
-//     }
-
-//     // Configure system clock and bus dividers
-//     RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | 
-//                                    RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | 
-//                                    RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_D3PCLK1);
-    
-//     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-//     RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-//     RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-//     RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-//     RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-//     RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-//     RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
-    
-//     ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
-//     if(ret != HAL_OK) {
-//         while(1) {} // Error handler
-//     }
-// }
-
-
-/**
  * @brief Configure GPIO for LEDs
  */
 void yellow_led_init(void)
@@ -223,19 +162,7 @@ void setup()
     // HAL initialization
     HAL_Init();
     
-    // Configure system clock
-    // SystemClock_Config();
-    
-    // HAL_Delay uses SysTick which is configured by HAL_Init()
-    // SystemCoreClock is updated by SystemClock_Config(), so delays will be accurate
-    
-    // Enable I-Cache and D-Cache for better performance
-    // SCB_EnableICache();
-    // SCB_EnableDCache();
-
-    // Add your initialization code here
     // GPIO, UART, TIM, ETH configuration goes here
-    
     yellow_led_init();
     red_led_init(); 
 }
