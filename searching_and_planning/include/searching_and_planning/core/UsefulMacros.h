b1c761c58d2065ac868c32a9660bea9077403897
YuKang <yukang.kong.uni@gmail.com>
2026-02-08 11:54:20 -0700

    update marco to fit with test enviroument

--- searching_and_planning/include/searching_and_planning/core/UsefulMacros.h
+++ searching_and_planning/include/searching_and_planning/core/UsefulMacros.h
@@ -7,11 +7,12 @@
 #define GAP 0.05
 #define ROVER_RADIUS 0.15 // in meters
 #define RADIUS_INFLATION 1.5 // inflation for rover radius when adding path obstacles
-#define MAP_WIDTH 100 // in cells
-#define MAP_HEIGHT 100 // in cells
+#define MAP_WIDTH 440 // in cells
+#define MAP_HEIGHT 180 // in cells
 #define X_MIN 0.0 // in meters
-#define X_MAX 10.0 // in meters
+#define X_MAX 22.0 // in meters
 #define Y_MIN 0.0 // in meters
-#define Y_MAX 10.0 // in meters
+#define Y_MAX 9.0 // in meters
+// make sure that x and y have the same resolution
 
 #endif 
