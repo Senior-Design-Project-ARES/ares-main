7c7a3ae5a4a5b5ca46068fb51593014a41243d73
YuKang <yukang.kong.uni@gmail.com>
2026-03-25 10:58:06 -0600

    update topic name

--- path_tracking/src/pathTracking.cpp
+++ path_tracking/src/pathTracking.cpp
@@ -156,13 +156,13 @@ class PathTrackingNode : public rclcpp::Node
             sub_pos = this->create_subscription<geometry_msgs::msg::PoseStamped>("/current_pose", 10, std::bind(&PathTrackingNode::poseCallback, this, _1));
 
             // Subscribe to waypoints
-            sub_wp = this->create_subscription<nav_msgs::msg::Path>("/micro_waypoints", 10, std::bind(&PathTrackingNode::pathCallback, this, _1));
+            sub_wp = this->create_subscription<nav_msgs::msg::Path>("micro_waypoints", 10, std::bind(&PathTrackingNode::pathCallback, this, _1));
 
             // Subscribe to stopIF
-            sub_stop = this->create_subscription<std_msgs::msg::Bool>("/stop_rover", 10, std::bind(&PathTrackingNode::stopCallback, this, _1));
+            sub_stop = this->create_subscription<std_msgs::msg::Bool>("stop_rover", 10, std::bind(&PathTrackingNode::stopCallback, this, _1));
 
             // Publish to cmd_vel
-            pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+            pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
         }
 
     // Pose callback
