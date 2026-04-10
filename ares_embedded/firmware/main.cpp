4d42d3031cfcf45306a9d56034a7001b0cceb253
JoshCol <joco8633@colorado.edu>
2026-04-10 15:16:07 -0600

    Adding final ethernet changes that were left out

--- ares_embedded/firmware/main.cpp
+++ ares_embedded/firmware/main.cpp
@@ -3,16 +3,51 @@
  * @brief Minimal firmware entry point for STM32H723ZG
  * @author Vishnu Duriseti
  * Arduino-like workflow: setup() runs once, loop() runs forever
+ * Updated 3/6/26: Added Ethernet Configurations
  */
 
 #include "stm32h7xx.h"
 #include "stm32h7xx_hal.h"
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "lwip/timeouts.h"
+#include "netif/ethernet.h"
+#include "ethernetif.h"
+#include "lan8742.h"
+#include "lwipopts.h"
+
+// Ethernet Handler
+extern ETH_HandleTypeDef heth;
 
 extern "C" {
     void SystemClock_Config(void);
     void SystemInit(void);
     void ExitRun0Mode(void);
 }
+
+
+extern "C" int32_t ETH_PHY_ReadReg(uint32_t DevAddr, uint32_t RegAddr, uint32_t *pRegVal)
+{
+    return HAL_ETH_ReadPHYRegister(&heth, DevAddr, RegAddr, pRegVal);
+}
+extern "C" int32_t ETH_PHY_WriteReg(uint32_t DevAddr, uint32_t RegAddr, uint32_t RegVal)
+{
+    return HAL_ETH_WritePHYRegister(&heth, DevAddr, RegAddr, RegVal);
+}
+extern "C" int32_t ETH_GetTick(void)
+{
+    return (int32_t)HAL_GetTick();
+}
+    
+extern "C" void Error_Handler(void)
+{
+    while (1);
+}
+extern ETH_DMADescTypeDef DMARxDscrTab[];
+extern ETH_DMADescTypeDef DMATxDscrTab[];
+extern uint8_t Rx_Buff[][1524];
+
+extern lan8742_Object_t LAN8742;
 
 // LED GPIO definitions for NUCLEO-H723ZG
 // LD2 (Yellow): PE1
@@ -25,6 +60,7 @@ extern "C" {
 // Global timer handle for red LED PWM
 TIM_HandleTypeDef htim12_red_led = {};
 
+struct netif gnetif;
 
 /**
  * @brief Configure GPIO for LEDs
@@ -153,6 +189,123 @@ void red_led_init(void)
     HAL_TIM_PWM_Start(&htim12_red_led, TIM_CHANNEL_1);
 }
 
+/**
+ * Configuring the Pins for ethenet connection
+ */
+void ETH_GPIO_init(void){
+    //enabling clocks
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+
+    GPIO_InitTypeDef g = {};
+    g.Mode = GPIO_MODE_AF_PP;
+    g.Pull = GPIO_NOPULL;
+    g.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+    g.Alternate = GPIO_AF11_ETH;
+
+    //Enabling Pins
+    g.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
+    HAL_GPIO_Init(GPIOA, &g);
+    g.Pin = GPIO_PIN_13;
+    HAL_GPIO_Init(GPIOB, &g);
+    g.Pin = GPIO_PIN_1 | GPIO_PIN_4 | GPIO_PIN_5;
+    HAL_GPIO_Init(GPIOC, &g);
+    g.Pin = GPIO_PIN_11 | GPIO_PIN_12;
+    HAL_GPIO_Init(GPIOB, &g);
+}
+
+
+/**
+ * Ethernet Initialization
+ */
+void ETH_Init(void){
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    HAL_SYSCFG_ETHInterfaceSelect(SYSCFG_ETH_RMII);
+    //memory mapping hardware registers
+    heth.Instance = ETH;
+    
+    //defining MAC address
+    static uint8_t mac[6] = {0x02,0x80,0xE1,0x00,0x00,0x01};
+    
+    //telling HAL driver the address type is mac
+    heth.Init.MACAddr = mac;
+    heth.Init.MediaInterface = HAL_ETH_RMII_MODE;
+
+    //setting up buffer for incoming packets
+    heth.Init.RxDesc = DMARxDscrTab;
+    heth.Init.TxDesc = DMATxDscrTab;
+    heth.Init.RxBuffLen = 1524;
+
+
+    //Enable Clocks
+    __HAL_RCC_ETH1MAC_CLK_ENABLE();
+    __HAL_RCC_ETH1TX_CLK_ENABLE();
+    __HAL_RCC_ETH1RX_CLK_ENABLE();
+
+    //Calling Ethernet initialization & checking errors
+    if (HAL_ETH_Init(&heth) != HAL_OK)
+    {
+        while(1); 
+    }
+
+    if(!(ETH->DMAMR & ETH_DMAMR_SWR))
+    {
+        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_1, GPIO_PIN_SET);
+    }
+
+    lan8742_IOCtx_t ioctx;
+
+    ioctx.Init       = NULL;
+    ioctx.DeInit     = NULL;
+    ioctx.ReadReg    = ETH_PHY_ReadReg;
+    ioctx.WriteReg   = ETH_PHY_WriteReg;
+    ioctx.GetTick    = ETH_GetTick;
+
+    //Calling Peripheral initialization
+    LAN8742_RegisterBusIO(&LAN8742, &ioctx);
+    LAN8742.DevAddr = 1;
+    LAN8742_Init(&LAN8742);
+
+    //check for link connection
+    int32_t link = LAN8742_GetLinkState(&LAN8742);
+    if(link <= LAN8742_STATUS_LINK_DOWN)
+    {
+        Error_Handler();
+    }
+
+    HAL_ETH_Start(&heth);
+}
+
+
+/**
+ * Ethernet Stack Initialization
+ */
+void ETH_Stack_Init()
+{
+    ip4_addr_t ipaddr;
+    ip4_addr_t netmask;
+    ip4_addr_t gw;
+
+    IP4_ADDR(&ipaddr, 10,42,0,100);
+    IP4_ADDR(&netmask,255,255,255,0);
+    IP4_ADDR(&gw,10,42,0,1);
+
+    lwip_init();
+
+    netif_add(&gnetif,
+          &ipaddr,
+          &netmask,
+          &gw,
+          NULL,
+          ethernetif_init,
+          ethernet_input);
+
+    netif_set_default(&gnetif);
+    netif_set_up(&gnetif);
+    netif_set_link_up(&gnetif);
+}
 
 /**
  * @brief Setup function - runs once at startup (like Arduino setup())
@@ -161,10 +314,25 @@ void setup()
 {
     // HAL initialization
     HAL_Init();
+    SCB_DisableDCache();
+
+    // Enabling the Cache for Ethernet
+    SCB_EnableICache();
+    //SCB_EnableDCache();
+
+    // Clock configuration
+    SystemClock_Config();
     
     // GPIO, UART, TIM, ETH configuration goes here
     yellow_led_init();
     red_led_init(); 
+
+    // Ethernet Initializations
+    ETH_GPIO_init();
+    ETH_Init();
+    uint32_t mac = ETH->MACCR;
+    ETH_Stack_Init();    
+
 }
 
 /**
@@ -177,8 +345,12 @@ void setup()
  */
 void loop()
 {
-    // This runs continuously after setup()
+    //Ethernet
+    ethernetif_input(&gnetif);
+    sys_check_timeouts();
 
+
+    /* Temporarily Disabled for Ethernet testing
     // Turn off Yellow LED, fade Red LED in and out using PWM
     HAL_GPIO_WritePin(LED_YELLOW_PORT, LED_YELLOW_PIN, GPIO_PIN_RESET); // Yellow LED off
     
@@ -205,6 +377,7 @@ void loop()
     HAL_Delay(5000);  
     
     // Cycle repeats (total 10 seconds per cycle)
+    */
 }
 
 /**
@@ -227,3 +400,4 @@ extern "C" {
         // No C++ global constructors needed for embedded
     }
 }
+
