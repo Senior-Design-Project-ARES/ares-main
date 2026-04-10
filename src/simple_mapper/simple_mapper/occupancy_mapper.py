6f5c9f2d69c1ff5858a57cc0c770f441a32d2d22
YuKang <yukang.kong.uni@gmail.com>
2026-04-10 11:34:50 -0600

    debug logger

--- src/simple_mapper/simple_mapper/occupancy_mapper.py
+++ src/simple_mapper/simple_mapper/occupancy_mapper.py
@@ -219,7 +219,7 @@ class OccupancyMapper(Node):
 
     def on_scan(self, scan: LaserScan):
         """Buffer scan for later processing."""
-        self.get_logger().logdebug(f'Received scan with timestamp {stamp_to_sec(scan.header.stamp):.3f}')  
+        self.get_logger().info(f'Received scan with timestamp {stamp_to_sec(scan.header.stamp):.3f}')  
         self.scan_buffer.append(scan)
 
     # -----------------------------------------------------------------------
