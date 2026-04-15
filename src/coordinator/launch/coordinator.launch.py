a595399a196c9a859a092c0764e9ff99b0aea367
User-YuKang <yukang.kong.uni@gmail.com>
2026-04-15 17:23:18 -0600

    add launching lidar into coordinator launch

--- src/coordinator/launch/coordinator.launch.py
+++ src/coordinator/launch/coordinator.launch.py
@@ -5,28 +5,51 @@ from ament_index_python.packages import get_package_share_directory
 import yaml
 
 
+from launch.actions import GroupAction, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import PushRosNamespace
+
+
 def generate_launch_description():
     package_dir = get_package_share_directory('coordinator')
     config_path = os.path.join(package_dir, '..', '..', '..', '..', 'config', 'config.yaml')
-    # config_path = os.path.join(os.getcwd(), 'config', 'config.yaml')
 
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
 
     common_params = config.get('common_params', {})
-
     vrpn_parms = config.get('vrpn', {})
-
     path_tracking_params = config.get('path_tracking', {})
-    
+    lidar_params = config.get('simple_mapper', {})
+
+
+    rplidar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('rplidar_ros'),
+                'launch',
+                'rplidar_a2m12_launch.py'
+            )
+        )
+    )
+
+    mapper = Node(
+        package='simple_mapper',
+        executable='occupancy_mapperV1',
+        name='occupancy_mapperV1',
+        output='screen',
+        parameters=[common_params, lidar_params]
+
+    )
+
     return LaunchDescription([
-        # Node(
-        #     package='vrpn_client_ros',
-        #     executable='vrpn_client_node',
-        #     output='screen',
-        #     emulate_tty=True,
-        #     parameters=[common_params, vrpn_parms],
-        # ),
+        Node(
+            package='vrpn_client_ros',
+            executable='vrpn_client_node',
+            output='screen',
+            emulate_tty=True,
+            parameters=[common_params, vrpn_parms],
+        ),
         Node(
             package='coordinator',
             executable='coordinator_node',
@@ -69,4 +92,9 @@ def generate_launch_description():
             output='screen',
             parameters=[common_params, path_tracking_params]
         ),
+        GroupAction([
+            PushRosNamespace(common_params.get('rover_name', 'Dora')),
+            rplidar,
+            mapper
+        ])
     ])
